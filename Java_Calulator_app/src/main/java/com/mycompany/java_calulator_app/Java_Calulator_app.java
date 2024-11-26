799627d621094c0c603515a58fd255858fa3e301
Lakshitha <IT22172082@my.sliit.lk>
2024-11-26 22:16:47 +0530

    Add min,mul,div ,add buttons

--- Java_Calulator_app/src/main/java/com/mycompany/java_calulator_app/Java_Calulator_app.java
+++ Java_Calulator_app/src/main/java/com/mycompany/java_calulator_app/Java_Calulator_app.java
@@ -7,12 +7,15 @@ package com.mycompany.java_calulator_app;
 import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Menu;
 import java.awt.MenuBar;
 import java.awt.MenuItem;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -41,23 +44,28 @@ class calbody extends JFrame {
         BorderLayout bd = new BorderLayout();
         
         JPanel p1 = new JPanel();
-        p1.setBackground(Color.red);
+        p1.setBackground(Color.DARK_GRAY);
         JPanel p2 = new JPanel();
         p2.setBackground(Color.blue);
         
         add(p1,BorderLayout.NORTH);
         add(p2,BorderLayout.CENTER);
         
-        TextField t1 = new TextField(30);
+        TextField t1 = new TextField(15);
+        t1.setFont(new Font("", 0, 30));
         p1.add(t1);
         
         //create grid
         GridLayout gl = new GridLayout(3,3);
         p2.setLayout(gl);
         
-        Button b1 = new Button();
-        b1.setLabel("1");
+        Button b1 = new Button("1");
         p2.add(b1);
+        
+           b1.addActionListener(new ActionListener(){
+   public void actionPerformed(ActionEvent e) {
+        t1.setText("1");
+   }});
         
         Button b2 = new Button("2");
         p2.add(b2);
@@ -77,11 +85,45 @@ class calbody extends JFrame {
         Button b7 = new Button("7");
         p2.add(b7);
         
-        Button b8 = new Button("");
+        Button b8 = new Button("8");
         p2.add(b8);
         
         Button b9 = new Button("9");
         p2.add(b9);
+        
+        Button b0 = new Button("0");
+        p2.add(b0);
+        
+        Button plus = new Button("+");
+        p2.add(plus);
+        
+        Button min = new Button("-");
+        p2.add(min);
+        
+        Button mul = new Button("*");
+        p2.add(mul);
+        
+        Button dev = new Button("/");
+        p2.add(dev);
+        
+        Button clr = new Button("CE");
+        p2.add(clr);
+        
+        b1.setFont(new Font("", 0, 30));
+          b2.setFont(new Font("", 0, 30));
+            b3.setFont(new Font("", 0, 30));
+              b4.setFont(new Font("", 0, 30));
+                b5.setFont(new Font("", 0, 30));
+                  b6.setFont(new Font("", 0, 30));
+                    b7.setFont(new Font("", 0, 30));
+                      b8.setFont(new Font("", 0, 30));
+                        b9.setFont(new Font("", 0, 30));
+                          b0.setFont(new Font("", 0, 30));
+                            plus.setFont(new Font("", 0, 30));
+                              min.setFont(new Font("", 0, 30));
+                                mul.setFont(new Font("", 0, 30));
+                                  dev.setFont(new Font("", 0, 30));
+                                    clr.setFont(new Font("", 0, 30));
         
         MenuBar mb = new MenuBar();
         setMenuBar(mb);
@@ -101,5 +143,8 @@ class calbody extends JFrame {
     m1.add(m_new);
     m2.add(m_save);
     m3.add(m_Exit);
-}
+    
+
+   
+    }
 }
