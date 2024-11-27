c44d9896343c7302b8370b7c0a290582b25fd286
Lakshitha <IT22172082@my.sliit.lk>
2024-11-27 23:15:57 +0530

    modify the code

--- Java_Calulator_app/src/main/java/com/mycompany/java_calulator_app/Java_Calulator_app.java
+++ Java_Calulator_app/src/main/java/com/mycompany/java_calulator_app/Java_Calulator_app.java
@@ -1,14 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.java_calulator_app;
 
 import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Menu;
 import java.awt.MenuBar;
@@ -17,174 +12,171 @@ import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-/**
- *
- * @author Chamidu
- */
 public class Java_Calulator_app {
 
     public static void main(String[] args) {
-        
         calbody cal = new calbody();
-        
-        
     }
 }
+
 class calbody extends JFrame {
-    calbody(){
-        
+    double firstNumber, secondNumber, result;
+    String operator;
+
+    calbody() {
         setVisible(true);
         setTitle("Calculator");
-        setSize(300,400);
+        setSize(300, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        
+
         BorderLayout bd = new BorderLayout();
-        
+        setLayout(bd);
+
         JPanel p1 = new JPanel();
         p1.setBackground(Color.DARK_GRAY);
         JPanel p2 = new JPanel();
-        p2.setBackground(Color.blue);
-        
-        add(p1,BorderLayout.NORTH);
-        add(p2,BorderLayout.CENTER);
-        
+        p2.setBackground(Color.BLUE);
+
+        add(p1, BorderLayout.NORTH);
+        add(p2, BorderLayout.CENTER);
+
         TextField t1 = new TextField(15);
         t1.setFont(new Font("", 0, 30));
         p1.add(t1);
-        
-        //create grid
-        GridLayout gl = new GridLayout(3,3);
+
+        // Create grid layout for buttons
+        GridLayout gl = new GridLayout(4, 4); // Adjusted for extra row for operators
         p2.setLayout(gl);
-        
-        Button b1 = new Button("1");
-        p2.add(b1);
-        
-           b1.addActionListener(new ActionListener(){
-   public void actionPerformed(ActionEvent e) {
-          t1.setText(t1.getText() + "1");
-   }});
-        
-        Button b2 = new Button("2");
-        p2.add(b2);
-        b2.addActionListener(new ActionListener(){
-   public void actionPerformed(ActionEvent e) {
-          t1.setText(t1.getText() + "2");
-   }});
-        
-        Button b3 = new Button("3");
-        p2.add(b3);
-        b3.addActionListener(new ActionListener(){
-   public void actionPerformed(ActionEvent e) {
-          t1.setText(t1.getText() + "3");
-   }});
-        
-        Button b4 = new Button("4");
-        p2.add(b4);
-        b4.addActionListener(new ActionListener(){
-   public void actionPerformed(ActionEvent e) {
-          t1.setText(t1.getText() + "4");
-   }});
-        
-        Button b5 = new Button("5");
-        p2.add(b5);
-        b5.addActionListener(new ActionListener(){
-   public void actionPerformed(ActionEvent e) {
-          t1.setText(t1.getText() + "5");
-   }});
-        
-        Button b6 = new Button("6");
-        p2.add(b6);
-        b6.addActionListener(new ActionListener(){
-   public void actionPerformed(ActionEvent e) {
-          t1.setText(t1.getText() + "6");
-   }});
-        
-        Button b7 = new Button("7");
-        p2.add(b7);
-        b7.addActionListener(new ActionListener(){
-   public void actionPerformed(ActionEvent e) {
-          t1.setText(t1.getText() + "7");
-   }});
-        
-        Button b8 = new Button("8");
-        p2.add(b8);
-        b8.addActionListener(new ActionListener(){
-   public void actionPerformed(ActionEvent e) {
-          t1.setText(t1.getText() + "8");
-   }});
-        
-        Button b9 = new Button("9");
-        p2.add(b9);
-        b9.addActionListener(new ActionListener(){
-   public void actionPerformed(ActionEvent e) {
-          t1.setText(t1.getText() + "9");
-   }});
-        
-        Button b0 = new Button("0");
-        p2.add(b0);
-        b0.addActionListener(new ActionListener(){
-   public void actionPerformed(ActionEvent e) {
-          t1.setText(t1.getText() + "0");
-   }});
-        
-        Button plus = new Button("+");
-        p2.add(plus);
-        
-        Button min = new Button("-");
-        p2.add(min);
-        
-        Button mul = new Button("*");
-        p2.add(mul);
-        
-        Button dev = new Button("/");
-        p2.add(dev);
-        
+
+        // Number buttons
+        Button[] numberButtons = new Button[10];
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new Button(String.valueOf(i));
+            numberButtons[i].setFont(new Font("", 0, 30));
+            numberButtons[i].addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    t1.setText(t1.getText() + ((Button) e.getSource()).getLabel());
+                }
+            });
+            p2.add(numberButtons[i]);
+        }
+
+        // Special buttons
+        Button dot = new Button(".");
+        dot.setFont(new Font("", 0, 30));
+        dot.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (!t1.getText().contains(".")) {
+                    t1.setText(t1.getText() + ".");
+                }
+            }
+        });
+        p2.add(dot);
+
         Button clr = new Button("CE");
+        clr.setFont(new Font("", 0, 30));
+        clr.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                t1.setText("");
+                firstNumber = 0.0;
+                secondNumber = 0.0;
+                operator = "";
+            }
+        });
         p2.add(clr);
-        clr.addActionListener(new ActionListener(){
-   public void actionPerformed(ActionEvent e) {
-          t1.setText("");
-   }});
-        
-        b1.setFont(new Font("", 0, 30));
-          b2.setFont(new Font("", 0, 30));
-            b3.setFont(new Font("", 0, 30));
-              b4.setFont(new Font("", 0, 30));
-                b5.setFont(new Font("", 0, 30));
-                  b6.setFont(new Font("", 0, 30));
-                    b7.setFont(new Font("", 0, 30));
-                      b8.setFont(new Font("", 0, 30));
-                        b9.setFont(new Font("", 0, 30));
-                          b0.setFont(new Font("", 0, 30));
-                            plus.setFont(new Font("", 0, 30));
-                              min.setFont(new Font("", 0, 30));
-                                mul.setFont(new Font("", 0, 30));
-                                  dev.setFont(new Font("", 0, 30));
-                                    clr.setFont(new Font("", 0, 30));
-        
+
+        // Operator buttons
+        Button[] operatorButtons = new Button[4];
+        String[] operators = {"+", "-", "*", "/"};
+        for (int i = 0; i < 4; i++) {
+            operatorButtons[i] = new Button(operators[i]);
+            operatorButtons[i].setFont(new Font("", 0, 30));
+            operatorButtons[i].addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if (!t1.getText().isEmpty()) {
+                        firstNumber = Double.parseDouble(t1.getText());
+                        operator = ((Button) e.getSource()).getLabel();
+                        t1.setText("");
+                    }
+                }
+            });
+            p2.add(operatorButtons[i]);
+        }
+
+        // Equals button
+        Button equals = new Button("=");
+        equals.setFont(new Font("", 0, 30));
+        equals.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (!t1.getText().isEmpty()) {
+                    secondNumber = Double.parseDouble(t1.getText());
+                    result = calculate(firstNumber, secondNumber, operator);
+                    t1.setText(String.valueOf(result));
+                    firstNumber = 0.0;
+                    secondNumber = 0.0;
+                    operator = "";
+                }
+            }
+        });
+        p2.add(equals);
+
+        // Set fonts for all buttons
+        for (Button btn : numberButtons) {
+            btn.setFont(new Font("", 0, 30));
+        }
+        for (Button btn : operatorButtons) {
+            btn.setFont(new Font("", 0, 30));
+        }
+
+        // Menu bar setup
         MenuBar mb = new MenuBar();
         setMenuBar(mb);
-        
+
         Menu m1 = new Menu("File");
         Menu m2 = new Menu("Edit");
         Menu m3 = new Menu("View");
         mb.add(m1);
         mb.add(m2);
         mb.add(m3);
-    
-    
-    MenuItem m_new = new MenuItem("New File");
-    MenuItem m_save = new MenuItem("Save");
-    MenuItem m_Exit = new MenuItem("Exit");
-    
-    m1.add(m_new);
-    m2.add(m_save);
-    m3.add(m_Exit);
-    
 
-   
+        MenuItem m_new = new MenuItem("New File");
+        MenuItem m_save = new MenuItem("Save");
+        MenuItem m_exit = new MenuItem("Exit");
+
+        m1.add(m_new);
+        m2.add(m_save);
+        m3.add(m_exit);
+
+        m_exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(rootPane, "Exiting Calculator...");
+                System.exit(0);
+            }
+        });
+    }
+
+    public double calculate(double a, double b, String operator) {
+        switch (operator) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                if (b != 0) {
+                    return a / b;
+                } else {
+                    JOptionPane.showMessageDialog(this, "Error: Division by zero!");
+                    return 0;
+                }
+            default:
+                return 0;
+        }
     }
 }
